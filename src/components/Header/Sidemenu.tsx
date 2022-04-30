e017b080e48a3dd4ad0e3c1233d1500b7155b651
Naing <naing.work.it@gmail.com>
2022-04-30 11:43:25 +0630

    adding close btn for sidemenu

--- src/components/Header/Sidemenu.tsx
+++ src/components/Header/Sidemenu.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+
 import { Modal } from './Modal';
 
 type SidemenuProps = { isOpen: boolean; close: () => void };
 
 export const Sidemenu: React.FC<SidemenuProps> = ({ isOpen, close }) => {
+  const navigate = useNavigate();
+
   return (
     <Modal>
       <>
@@ -15,35 +19,43 @@ export const Sidemenu: React.FC<SidemenuProps> = ({ isOpen, close }) => {
           }`}
         ></div>
         <div
-          className={`absolute inset-y-0 z-50 w-3/4 max-w-xs bg-blue-900 transition-transform duration-300 md:hidden ${
+          className={`absolute inset-y-0 z-50 w-3/5 max-w-[280px] bg-white transition-transform duration-300 md:hidden ${
             isOpen ? '' : '-translate-x-full'
           }`}
         >
-          <h3 className="m-4 text-center font-medium text-white">CryptoHub</h3>
-          <ul className="border-t-2 border-t-white">
-            <li>
-              <Link
-                className="inline-block w-full p-2 text-white hover:bg-blue-400"
-                to={'/'}
-              >
-                Home
-              </Link>
+          <div className="flex items-center bg-blue-900">
+            <button onClick={close} className="m-2 p-2 text-xl text-white">
+              <AiOutlineClose />
+            </button>
+            <h3 className="text-white">CryptoHub</h3>
+          </div>
+          <ul className="mt-6 p-2">
+            <li
+              onClick={() => {
+                close();
+                navigate('/');
+              }}
+              className="w-full cursor-pointer p-2 text-blue-900 hover:bg-blue-300"
+            >
+              Home
             </li>
-            <li>
-              <Link
-                className="inline-block w-full p-2 text-white hover:bg-blue-400"
-                to={'/cryptocurrencies'}
-              >
-                Cryptocurrencies
-              </Link>
+            <li
+              onClick={() => {
+                close();
+                navigate('/cryptocurrencies');
+              }}
+              className="w-full cursor-pointer p-2 text-blue-900 hover:bg-blue-300"
+            >
+              Cryptocurrencies
             </li>
-            <li>
-              <Link
-                className="inline-block w-full p-2 text-white hover:bg-blue-400"
-                to={'/about'}
-              >
-                About
-              </Link>
+            <li
+              onClick={() => {
+                close();
+                navigate('/about');
+              }}
+              className="w-full cursor-pointer p-2 text-blue-900 hover:bg-blue-300"
+            >
+              About
             </li>
           </ul>
         </div>
